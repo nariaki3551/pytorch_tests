4db5efbd31435fa4187a6387b3221b8933596d91
Nariaki Tateiwa <nariaki3551@gmail.com>
2025-04-22 13:43:39 +0000

    add reducescatter

--- src/mpi/test_reducescatter_cuda.cpp
+++ src/mpi/test_reducescatter_cuda.cpp
@@ -13,24 +13,24 @@ struct Context {
     int* recv_buff;
 };
 
-double do_allgather(Context *ctx) {
+double do_reduce_scatter(Context *ctx) {
     double elapsed_time = - MPI_Wtime();
-    MPI_Allgather(
-        ctx->send_buff, ctx->count, MPI_INT,
-        ctx->recv_buff, ctx->count, MPI_INT,
-        MPI_COMM_WORLD
+    MPI_Reduce_scatter_block(
+        ctx->send_buff, ctx->recv_buff, ctx->count,
+        MPI_INT, MPI_SUM,
+	MPI_COMM_WORLD
     );
     elapsed_time += MPI_Wtime();
     return elapsed_time;
 }
 
-double do_iallgather(Context *ctx) {
+double do_ireduce_scatter(Context *ctx) {
     MPI_Request request;
     double elapsed_time = - MPI_Wtime();
-    MPI_Iallgather(
-        ctx->send_buff, ctx->count, MPI_INT,
-        ctx->recv_buff, ctx->count, MPI_INT,
-        MPI_COMM_WORLD, &request
+    MPI_Ireduce_scatter_block(
+        ctx->send_buff, ctx->recv_buff, ctx->count,
+        MPI_INT, MPI_SUM,
+	MPI_COMM_WORLD, &request
     );
     MPI_Wait(&request, MPI_STATUS_IGNORE);
     elapsed_time += MPI_Wtime();
@@ -38,18 +38,15 @@ double do_iallgather(Context *ctx) {
 }
 
 void check_recv_buff(Context *ctx, int* host_buff) {
-    // assert that the recv_buff is the same as the send_buff
-    cudaMemcpy(host_buff, ctx->recv_buff, ctx->count * ctx->size * sizeof(int), cudaMemcpyDeviceToHost);
-
-    int i = 0;
-    for (int rank = 0; rank < ctx->size; ++rank) {
-        for (int k = 0; k < ctx->count; ++k) {
-            if (host_buff[i] != rank) {
-                fprintf(stderr, "rank: %d, recv_buff[%d] is %d, expected %d\n", ctx->rank, i, ctx->recv_buff[i], rank);
-                fflush(stderr);
-                abort();
-            }
-            i++;
+    cudaMemcpy(host_buff, ctx->recv_buff, ctx->count * sizeof(int), cudaMemcpyDeviceToHost);
+    int sum = 0;
+    for (int i = 0; i < ctx->size; ++i) {
+        sum += i;
+    }
+    for (int i = 0; i < ctx->count; ++i) {
+        if (host_buff[i] != sum) {
+            printf("rank: %d, recv_buff[%d] is %d, expected %d\n", ctx->rank, i, host_buff[i], sum);
+            abort();
         }
     }
 }
@@ -82,24 +79,23 @@ int main(int argc, char** argv) {
     // Using cudaMalloc, we get the following error, so use cudaMallocHost
     //     ERROR ibv_reg_mr(addr:0x7f785ba00000 size:8192) failed: Bad address)
     //     tl_sharp_coll.c:111  TL_SHARP ERROR ucc_rcache_get failed
-    cudaMallocHost((void**)&ctx.send_buff, ctx.count * sizeof(int));
-    cudaMallocHost((void**)&ctx.recv_buff, ctx.count * ctx.size * sizeof(int));
+    cudaMallocHost((void**)&ctx.send_buff, ctx.count * ctx.size * sizeof(int));
+    cudaMallocHost((void**)&ctx.recv_buff, ctx.count * sizeof(int));
 
     int* host_buff = (int*)malloc(ctx.count * ctx.size * sizeof(int));
-    for (int i = 0; i < ctx.count; ++i) {
+    for (int i = 0; i < ctx.count * ctx.size; ++i) {
         host_buff[i] = ctx.rank;
     }
-    cudaMemcpy(ctx.send_buff, host_buff, ctx.count * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(ctx.send_buff, host_buff, ctx.count * ctx.size * sizeof(int), cudaMemcpyHostToDevice);
 
-
-    printf("rank %d: start iallgather with count %d\n", ctx.rank, ctx.count);
-    elapsed_time = do_iallgather(&ctx);
-    printf("rank %d: end iallgather, time: %f\n", ctx.rank, elapsed_time);
+    printf("rank %d: start reducescatter with count %d\n", ctx.rank, ctx.count);
+    elapsed_time = do_reduce_scatter(&ctx);
+    printf("rank %d: end reducescatter, time: %f\n", ctx.rank, elapsed_time);
     check_recv_buff(&ctx, host_buff);
 
-    printf("rank %d: start allgather with count %d\n", ctx.rank, ctx.count);
-    elapsed_time = do_allgather(&ctx);
-    printf("rank %d: end allgather, time: %f\n", ctx.rank, elapsed_time);
+    printf("rank %d: start ireducescatter with count %d\n", ctx.rank, ctx.count);
+    elapsed_time = do_ireduce_scatter(&ctx);
+    printf("rank %d: end ireducescatter, time: %f\n", ctx.rank, elapsed_time);
     check_recv_buff(&ctx, host_buff);
 
     printf("rank %d: success\n", ctx.rank);
