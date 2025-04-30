33354ecb820b525c7b813c8ecd4cb0f5be0efa0a
Nariaki Tateiwa <nariaki3551@gmail.com>
2025-04-30 16:42:08 +0900

    update test_allgather.py (#2)

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 .PHONY: run_test_allgather run_test_fsdp run_cuda_support_check_mpi run_check_ucc_sharp_support
 
+all: build_mpi build_ucc
+
 build_mpi:
 	$(MAKE) -C ./src/mpi
 
@@ -7,15 +9,7 @@ build_ucc:
 	$(MAKE) -C ./src/ucc
 
 run_test_allgather:
-	mpirun -n 2 python3 ./src/collective/test_allgather.py
-
-run_test_fsdp_mpi:
-	mpirun -n 2 --allow-run-as-root python3 ./src/training/test_fsdp.py --model_scale 1 --num_epochs 2
-
-run_test_fsdp_gloo:
-	TORCH_CPP_LOG_LEVEL=INFO TORCH_DISTRIBUTED_DEBUG=DETAIL OMP_NUM_THREADS=1 \
-		torchrun --nproc-per-node 2 --master_addr="0.0.0.0" --master_port=23456 \
-		-- ./src/training/test_fsdp.py --model_scale 1 --num_epochs 2 --backend gloo
+	$(MAKE) -C ./src/collective run_all
 
 run_test_mpi: build_mpi
 	$(MAKE) -C ./src/mpi run_test
--- src/collective/test_allgather.py
+++ src/collective/test_allgather.py
@@ -1,4 +1,6 @@
+import os
 import argparse
+
 import torch
 import torch.distributed as dist
 
@@ -11,9 +13,9 @@ def main(args):
     print(f"Rank {rank}/{size_}: allgather test with count {count} and device {device}")
 
     source = [rank for _ in range(count)]
-    # input_tensor = torch.ones(count, dtype=torch.int16, device=device) * rank
-    input_tensor = torch.tensor(source, dtype=torch.int16, device=device)
-    output_tensor = torch.zeros(count * size_, dtype=torch.int16, device=device)
+    # input_tensor = torch.ones(count, dtype=torch.int32, device=device) * rank
+    input_tensor = torch.tensor(source, dtype=torch.int32, device=device)
+    output_tensor = torch.zeros(count * size_, dtype=torch.int32, device=device)
 
     print(f"Rank {rank} allgather input: {input_tensor}")
     dist.all_gather_into_tensor(output_tensor, input_tensor)
@@ -21,7 +23,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Allgather example')
-    parser.add_argument('--backend', type=str, default='mpi', choices=['mpi', 'ucc', 'nccl'], help='Backend to use')
+    parser.add_argument('--backend', type=str, default='mpi', choices=['mpi', 'ucc', 'nccl', 'gloo'], help='Backend to use')
     parser.add_argument('--count', type=int, default=3, help='Number of elements in each tensor')
     parser.add_argument('--device', type=str, default='cpu', choices=['cpu', 'cuda'], help='Device to run the test on')
     args = parser.parse_args()
@@ -31,7 +33,16 @@ if __name__ == "__main__":
     elif args.backend == 'ucc':
         dist.init_process_group(backend='ucc', init_method='env://')
     elif args.backend == 'nccl':
+        assert args.device == 'cuda', "NCCL backend only supports CUDA device"
         dist.init_process_group(backend='nccl', init_method='env://')
+    elif args.backend == 'gloo':
+        assert args.device == 'cpu', "Gloo backend only supports CPU device"
+        dist.init_process_group(backend='gloo', init_method='env://')
+
+    if args.device == 'cuda':
+        local_rank = int(os.environ.get('LOCAL_RANK', 0))
+        args.device = f'cuda:{local_rank}'
+        torch.cuda.set_device(args.device)
 
     main(args)
 
