dcdb20d484dbfeaa35f67d7fc46b6530a4f48f0b
Nariaki Tateiwa <nariaki3551@gmail.com>
2025-07-01 06:22:08 +0000

    fix allgather and reducescatter perf

--- src/mpi/reducescatter_perf_cuda.cpp
+++ src/mpi/reducescatter_perf_cuda.cpp
@@ -15,15 +15,12 @@ struct Context {
 };
 
 double do_reduce_scatter(Context *ctx) {
-    MPI_Request request;
-    double elapsed_time = - MPI_Wtime();
     MPI_Reduce_scatter_block(
         ctx->send_buff, ctx->recv_buff, ctx->count,
         MPI_INT, MPI_SUM,
         MPI_COMM_WORLD
     );
-    elapsed_time += MPI_Wtime();
-    return elapsed_time;
+    return 0;
 }
 
 void check_recv_buff(Context *ctx) {
@@ -47,6 +44,7 @@ int main(int argc, char** argv) {
     int iterations = 20;
     int warmup = 5;
     double step_factor = 2.0;
+    bool check = false;
 
     int opt;
     while ((opt = getopt(argc, argv, "l:u:w:n:s:")) != -1) {
@@ -56,6 +54,7 @@ int main(int argc, char** argv) {
             case 'w': warmup = std::stoi(optarg); break;
             case 'n': iterations = std::stoi(optarg); break;
             case 's': step_factor = std::stod(optarg); break;
+            case 'c': check = true; break;
             default:
                 fprintf(stderr, "Usage: %s [-l min_count] [-u max_count] [-w warmup] [-n iterations] [-s stepfactor]\n", argv[0]);
                 exit(EXIT_FAILURE);
@@ -74,34 +73,39 @@ int main(int argc, char** argv) {
     }
     fflush(stdout);
 
+    posix_memalign((void**)&ctx.send_buff, 4096, max_count * ctx.size * sizeof(int));
+    posix_memalign((void**)&ctx.recv_buff, 4096, max_count * sizeof(int));
+
     for (size_t count = min_count; count <= max_count; count = static_cast<size_t>(count * step_factor)) {
         ctx.count = static_cast<int>(count);
-        posix_memalign((void**)&ctx.send_buff, 4096, ctx.count * ctx.size * sizeof(int));
-        posix_memalign((void**)&ctx.recv_buff, 4096, ctx.count * sizeof(int));
-        for (int i = 0; i < ctx.count * ctx.size; ++i) {
-            ctx.send_buff[i] = ctx.rank;
+        if (check) {
+            for (int i = 0; i < ctx.count * ctx.size; ++i) {
+                ctx.send_buff[i] = ctx.rank;
+            }
         }
 
         for (int i = 0; i < warmup; ++i) {
             do_reduce_scatter(&ctx);
         }
 
-        double total_time = 0.0;
+        double elapsed_time = - MPI_Wtime();
         for (int i = 0; i < iterations; ++i) {
-            total_time += do_reduce_scatter(&ctx);
+            do_reduce_scatter(&ctx);
         }
+        elapsed_time += MPI_Wtime();
 
-        check_recv_buff(&ctx);
-        double avg_time = total_time / iterations;
+        if (check) {
+            check_recv_buff(&ctx);
+        }
+        double avg_time = elapsed_time / iterations;
         if (ctx.rank == 0) {
             printf("int\t%ld\t\t%.6f\t%.6f\n", count, count * sizeof(int) / 1024.0, avg_time);
         }
-        fflush(stdout);
 
-        free(ctx.send_buff);
-        free(ctx.recv_buff);
     }
 
+    free(ctx.send_buff);
+    free(ctx.recv_buff);
     double total_sent_words = 0;
     double total_received_words = 0;
     for (size_t count = min_count; count <= max_count; count = static_cast<size_t>(count * step_factor)) {
