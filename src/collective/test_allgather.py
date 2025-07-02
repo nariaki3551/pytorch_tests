8df0693560dffe74ff1c7c52a30bf1cf1c020b23
Nariaki Tateiwa <nariaki3551@gmail.com>
2025-07-02 01:08:14 +0000

    update

--- src/collective/test_allgather.py
+++ src/collective/test_allgather.py
@@ -9,23 +9,26 @@ def main(args):
     size_ = dist.get_world_size()
     count = args.count
     device = args.device
+    iterations = args.iterations
 
-    print(f"Rank {rank}/{size_}: allgather test with count {count} and device {device}")
+    print(f"Rank {rank}/{size_}: allgather test with count {count} and device {device} and iterations {iterations}")
 
     source = [rank for _ in range(count)]
     # input_tensor = torch.ones(count, dtype=torch.int32, device=device) * rank
     input_tensor = torch.tensor(source, dtype=torch.int32, device=device)
     output_tensor = torch.zeros(count * size_, dtype=torch.int32, device=device)
 
-    print(f"Rank {rank} allgather input: {input_tensor}")
-    dist.all_gather_into_tensor(output_tensor, input_tensor)
-    print(f"Rank {rank} allgather received: {output_tensor}")
+    for _ in range(iterations):
+        print(f"Rank {rank} allgather input: {input_tensor}")
+        dist.all_gather_into_tensor(output_tensor, input_tensor)
+        print(f"Rank {rank} allgather received: {output_tensor}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Allgather example')
     parser.add_argument('--backend', type=str, default='mpi', choices=['mpi', 'ucc', 'nccl', 'gloo'], help='Backend to use')
     parser.add_argument('--count', type=int, default=3, help='Number of elements in each tensor')
     parser.add_argument('--device', type=str, default='cpu', choices=['cpu', 'cuda'], help='Device to run the test on')
+    parser.add_argument('--iterations', type=int, default=1, help='Number of iterations to run the test')
     args = parser.parse_args()
 
     if args.backend == 'mpi':
